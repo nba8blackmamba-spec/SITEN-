9149603b27d49f723f27cbefcb8f2008ccb134a9
nba8blackmamba-spec <nba8blackmamba@gmail.com>
2026-07-28 15:00:31 +0900

    Create followers.js

---
+++ api/followers.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const token = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+
+  const listRes = await fetch("https://api.line.me/v2/bot/followers/ids", {
+    headers: { "Authorization": `Bearer ${token}` }
+  });
+  const listData = await listRes.json();
+  const ids = listData.userIds || [];
+
+  const profiles = [];
+  for (const id of ids) {
+    const profRes = await fetch(`https://api.line.me/v2/bot/profile/${id}`, {
+      headers: { "Authorization": `Bearer ${token}` }
+    });
+    if (profRes.ok) {
+      const prof = await profRes.json();
+      profiles.push({ userId: id, name: prof.displayName });
+    } else {
+      profiles.push({ userId: id, name: "(取得不可)" });
+    }
+  }
+
+  return res.status(200).json(profiles);
+}
